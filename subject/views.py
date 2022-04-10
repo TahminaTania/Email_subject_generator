f8662f6fbca69d2726d41c11a337154435d754bb
TahminaTania <1611414642@nothsouth.edu>
2022-04-11 00:13:55 +0600

    need to pass values

--- subject/views.py
+++ subject/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .models import subject
 # Create your views here.
 
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the view index.")
 
 
 def mail(request):
-    return render(request,'subject.html')
+    sub=subject.objects.all()
+    return render(request,'output.html',{
+        'sub': sub
+    })
