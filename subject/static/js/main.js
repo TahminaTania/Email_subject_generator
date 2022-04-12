014183ef54a50629d6553c2ece0d0036124e8273
TahminaTania <1611414642@northsouth.edu>
2022-04-12 13:48:00 +0600

    need to bold or style the input value.

--- subject/static/js/main.js
+++ subject/static/js/main.js
@@ -1,5 +1,12 @@
-function keyword(){
-    var input=document.getElementById("keyword").value
-    var word= document.getElementById("titl").value.replace('{}',input); 
+console.log("Connected")
+var input=document.getElementById("word").value;
+console.log("user value is",input)
 
-}
+
+// // function keyword(){
+//     console.log("input")
+//     var input=document.getElementById("word").value
+//     console.log("user value is",input)
+//     var word= document.getElementById("titl").value.replace('{}',input); 
+
+// // }
--- subject/views.py
+++ subject/views.py
@@ -1,27 +1,45 @@
-from multiprocessing.sharedctypes import Value
-from urllib import response
+from django import template
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import pin_point,benefit,topic
 # Create your views here.
 
+# register = template.Library()
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the view index.")
 
 
+# @register.filter
+# def to_and(value):
+#     return value.replace("{}","and")
+    
 def mail(request):
     sub=None
     cat = request.GET.get("category")
+    word=None
+    inp=request.GET.get("word")
+    
     if cat== "1":
         sub=benefit.objects.all()
+        for s in sub:
+            word= s.title.replace("{}",inp)
+            s.title=word
     elif cat=="2":
         sub=topic.objects.all()
+        for s in sub:
+            word= s.title.replace("{}",inp)
+            s.title=word
     elif cat=="3": 
-        sub=pin_point.objects.all()       
+        sub=pin_point.objects.all() 
+        for s in sub:
+            word= s.title.replace("{}",inp)
+            s.title=word      
       
     return render(request,'output.html',{
         'sub': sub,
-        'catagory':cat
+        'catagory':cat,
+        'word':word,
+        'inp':inp
     })
